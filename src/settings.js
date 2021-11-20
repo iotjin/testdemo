7409b964bed1335f19a5829563823b6ca59a66ea
iotjin <179608047@qq.com>
2021-11-20 18:06:22 +0800

    add

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  title: 'Jh Vue Admin',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true,
+  tagsView: true // tagsView显隐控制
+}
